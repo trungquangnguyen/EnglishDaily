716760f2b77c34a0d9e5309817596085e925bfd5
nguyen trung quang <trunhquang@gmail.com>
2016-03-12 13:46:24 +0700

    create new Project

---
+++ EnglishDaily/EnglishDaily/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EnglishDaily
+//
+//  Created by nguyen trung quang on 3/12/16.
+//  Copyright © 2016 sTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
